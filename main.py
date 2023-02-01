37b9a26aaa09179d23c3ef6a590574f3e8e6e785
SAURAV ROY <rsaurav81@yahoo.com>
2023-02-02 00:37:33 +0530

    Update main.py
    Added main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 
+import json
+import sys
+
+data1 = { "a": 
+            {"b": 
+            {"c":"d"
+        }
+    }
+ }
+
+searchkey=sys.argv[1]
+
+def get_value(myobject, key):
+    if type(myobject) == str:
+        myjson = json.loads(myobject)
+    if type(myobject) is dict:
+        for jsonkey in myobject:
+            if type(myobject[jsonkey]) in (list, dict):
+                get_value(myobject[jsonkey], key)
+            elif jsonkey == key:
+
+                print(myobject[jsonkey])
+
+    elif type(myobject) is list:
+        for item in myobject:
+            if type(item) in (list, dict):
+                get_value(item, key)
+
+get_value(data1,searchkey)
